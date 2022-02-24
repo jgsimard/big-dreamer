4cc8274407eac4087add6fb4d09d2e4c8686cc31
jaihon1 <jaihon.qasim@hotmail.com>
2022-02-24 15:05:18 -0500

    Setting up base project.

---
+++ pydreamer/scripts/example/install.sh
@@ -0,0 +1,10 @@
+## Install 
+
+brew install cmake zlib
+
+conda create -n pydreamer python=3.8 -y
+conda activate pydreamer
+pip install -r requirements.txt
+
+# pip install -e git+https://github.com/jurgisp/gym-minigrid.git#egg=gym-minigrid --src ../  # checkout for local development
+# pip install -e git+https://github.com/jurgisp/gym-miniworld.git#egg=gym-miniworld --src ../  # checkout for local development
