518270dee6117cb766f7e39c1b913433ddba77c0
jgsimard <jeangab23@gmail.com>
2022-03-31 01:28:15 -0400

    delete pydreamer

--- pydreamer/scripts/example/run_docker.sh
+++
@@ -1,5 +0,0 @@
-docker build . -f Dockerfile -t pydreamer --build-arg ENV=standard
-
-docker run -it \
-    pydreamer \
-    sh scripts/xvfb_run.sh python3 train.py --configs defaults dmc debug --run_name debug
